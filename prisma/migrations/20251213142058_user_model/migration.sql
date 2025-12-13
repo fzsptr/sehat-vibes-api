b16a7cb8dd12b15f31d55f75ae3a2dcafc4afb1c
Fauzi Saputra <karaktermobile1@gmail.com>
2025-12-13 21:22:15 +0700

    create user model

---
+++ prisma/migrations/20251213142058_user_model/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(100) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `weight` DOUBLE NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
