82521dac0705b75d074d68fea34666dadc0bf643
Fauzi Saputra <karaktermobile1@gmail.com>
2025-12-14 19:01:44 +0700

    added error middleware for error validation (400) and error internal (500)

---
+++ src/middleware/error-middleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response,NextFunction } from "express";
+import { ZodError } from "zod";
+import { ResponseError } from "../error/response-error";
+
+export const errorMiddleware = async(err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof ZodError) {
+        res.status(400).json({
+            status: "error",
+            errors: `Validation error ${JSON.stringify(err)}`
+        })
+    } else if (err instanceof ResponseError) {
+        res.status(err.status).json({
+            statis: "error",
+            errors: err.message
+        })
+    } else {
+        res.status(500).json({
+            status: "error",
+            errors: "Internal server error"
+        })
+    }
+}
