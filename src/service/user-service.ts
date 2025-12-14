4d872f858b83d0d01335e83e706047cd38d0b74a
Fauzi Saputra <karaktermobile1@gmail.com>
2025-12-14 18:37:16 +0700

    added hash pw using bcrypt, business logic service register, user validation and response error

---
+++ src/service/user-service.ts
@@ -0,0 +1,31 @@
+import { ResponseError } from "../error/response-error";
+import { prisma } from "../lib/database";
+import { toRegisterUserResponse } from "../mapper/user-mapper";
+import { RegisterUserRequest, RegisterUserResponse } from "../model/user-model";
+import { hashPassword } from "../utils/bcrypt";
+import { UserValidation } from "../validation/user-validation";
+import { Validation } from "../validation/validation";
+
+export class AuthService {
+    static async register(request: RegisterUserRequest) : Promise <RegisterUserResponse> {
+        const registerRequest = Validation.validate(UserValidation.REGISTER, request)
+
+        const existsUsername = await prisma.user.count({
+            where: {
+                username: registerRequest.username
+            }
+        })
+
+        if (existsUsername != 0) {
+            throw new ResponseError(400, "Username already exists")
+        }
+
+        registerRequest.password = await hashPassword(request.password)
+
+        const user = await prisma.user.create({
+            data: registerRequest
+        })
+
+        return toRegisterUserResponse(user)
+    }
+}
---
+++ src/validation/user-validation.ts
@@ -0,0 +1,12 @@
+import z, { ZodType } from "zod";
+import { RegisterUserRequest } from "../model/user-model";
+
+export class UserValidation {
+
+    static readonly REGISTER : ZodType<RegisterUserRequest> = z.object({
+        username: z.string().min(1).max(100),
+        name: z.string().min(1).max(100),
+        password: z.string().min(6).max(100),
+        weight: z.number().positive().optional()
+    })
+}
